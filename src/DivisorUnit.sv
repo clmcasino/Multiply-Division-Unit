8ff158c7b7170ae2b8a533affb8cb871f0e03a5e
Giuseppe <s255648@studenti.polito.it>
2019-08-01 10:57:09 +0200

    datapath completed with counter, asm evolution, output driver missing

--- src/DivisorUnit.sv
+++ src/DivisorUnit.sv
@@ -2,17 +2,19 @@ module DivisorUnit (clk,rst_n,valid,usigned_n,divisor,dividend,reminder,quotient
   parameter parallelism=32;
   input clk;
   input rst_n;
-
+  input valid;
+  input usigned_n;
   input [parallelism-1:0] divisor;
   input [parallelism-1:0] dividend;
   output [parallelism-1:0] reminder;
   output [parallelism-1:0] quotient;
   output res_ready;
+
   enum {  idle          =4'b0000,
           loadData      =4'b0001,
-          loadCnt       =4'b0010,
+          loadCnt1      =4'b0010,
           divisorLShift =4'b0011,
-          saveIterLookp =4'b0100,
+          saveIterLoop  =4'b0100,
           divKernelStep =4'b0101,
           computeQ      =4'b0110,
           waitSignals   =4'b0111,
@@ -23,4 +25,161 @@ module DivisorUnit (clk,rst_n,valid,usigned_n,divisor,dividend,reminder,quotient
           remCorrection =4'b1100,
           divDone       =4'b1101} present_state, next_state;
   //controSignals
-  
+  logic divisor_en;
+  logic divisor_lShift;
+  logic notDIvisor_en;
+  logic saveReminder;
+  logic sumHMux_sel;
+  logic sum_en;
+  logic carry_en;
+  logic [1:0] leftAddMux_sel;
+  logic [1:0] rightAddMux_sel;
+  logic QCorrectBitMux_sel;
+  logic leftAddMode;
+  logic rightAddMode;
+  logic reminder_en;
+  logic reminder_rShift;
+  logic quotient_en;
+  logic counterMux_sel;
+  logic count_upDown;
+  logic count_load;
+  logic count_en;
+  logic counterReg_en;
+  logic tc;
+  logic signS;
+  logic [1:0] magnitudeD;
+  logic signD;
+  logic signZ;
+  logic divisorReady;
+
+  //assigning signs
+  always_comb begin
+    if (usigned_n) begin
+      signZ=1'b0;
+      signD=1'b0;
+    end else begin
+      signZ=dividend[parallelism-1];
+      signD=divisor[parallelism-1];
+    end
+  end
+
+  //divisor magnitude
+  always_comb begin
+    if (magnitudeD[1] xor magnitudeD[0]) begin
+      divisorReady=1'b1;
+    end else begin
+      divisorReady=1'b0;
+    end
+  end
+
+  //instantiating the dp
+  DivisorUnitDP #(parallelism) divisorDP (.*);
+
+  //state transition
+  always_ff @ (posedge clk) begin
+    if (rst)
+      present_state<=idle;//reset synchronous
+    else
+      present_state<=next_state;
+  end
+
+
+
+  always_comb begin
+    case (present_state)
+      idle: if (valid) begin
+              next_state=loadData;
+            end else begin
+              next_state=idle;
+            end
+      loadData: if (divisorReady) begin
+                  next_state=loadCnt1;
+                end else begin
+                  next_state=divisorLShift;
+                end
+      loadCnt1: next_state=saveIterLoop;
+      divisorLShift:  if (divisorReady) begin
+                        next_state=saveIterLoop;
+                      end else begin
+                        next_state=divisorLShift;
+                      end
+      saveIterLoop: next_state=divKernelStep;
+      divKernelStep:  if (tc) begin
+                        next_state=computeQ;
+                      end else begin
+                        next_state=divKernelStep;
+                      end
+      computeQ: next_state=waitSignals;
+      waitSignals:  if (signS xor signZ) begin
+                      if (signS xor signD) begin
+                        next_state=correctDown;
+                      end else begin
+                        next_state=correctUp;
+                      end
+                    end else begin
+                      if (signD) begin
+                        next_state=qInv;
+                      end else begin
+                        next_state=loadIterLoop;
+                      end
+                    end
+      correctDown:  if (signD) begin
+                      next_state=qInv;
+                    end else begin
+                      next_state=loadIterLoop;
+                    end
+      correctUp:  if (signD) begin
+                    next_state=qInv;
+                  end else begin
+                    next_state=loadIterLoop;
+                  end
+      qInv: next_state=loadIterLoop;
+      loadIterLoop: next_state=remCorrection;
+      remCorrection: if (tc) begin
+                        next_state=divDone;
+                      end else begin
+                        next_state=remCorrection;
+                      end
+      divDone: next_state=idle;
+      default: next_state=idle;
+    endcase
+  end
+
+  always_comb begin
+    case (present_state)
+      idle:
+      loadData:
+      loadCnt1:
+      divisorLShift:
+      saveIterLoop:
+      divKernelStep:
+      computeQ:
+      waitSignals:
+      correctDown:
+      correctUp:
+      qInv:
+      loadIterLoop:
+      remCorrection:
+      divDone:      divisor_en=
+                    divisor_lShift=
+                    notDIvisor_en=
+                    saveReminder=
+                    sumHMux_sel=
+                    sum_en=
+                    carry_en=
+                    leftAddMux_sel=
+                    rightAddMux_sel=
+                    QCorrectBitMux_sel=
+                    leftAddMode=
+                    rightAddMode=
+                    reminder_en=
+                    reminder_rShift=
+                    quotient_en=
+                    counterMux_sel=
+                    count_upDown=
+                    count_load=
+                    count_en=
+                    counterReg_en=
+                    res_ready=     
+    endcase
+  end
--- src/DivisorUnitDP.sv
+++ src/DivisorUnitDP.sv
@@ -1,4 +1,4 @@
-module DivisorUnitDP (clk,rst_n,usigned_n,divisor,dividend,reminder,quotient,divisor_en,divisor_lShift,notDIvisor_en,save_reminder,sumHMux_sel,sum_en,carry_en,leftAddMux_sel,rightAddMux_sel,QCorrectBitMux_sel,reminder_en,reminder_rShift,quotient_en);
+module DivisorUnitDP (clk,rst_n,usigned_n,divisor,dividend,reminder,quotient,divisor_en,divisor_lShift,notDIvisor_en,save_reminder,sumHMux_sel,sum_en,carry_en,leftAddMux_sel,rightAddMux_sel,QCorrectBitMux_sel,reminder_en,reminder_rShift,quotient_en,counterMux_sel,count_upDown,count_load,count_en,counterReg_en,tc,signS,magnitudeD);
   parameter parallelism=32;
   input clk;
   input rst_n;
@@ -23,6 +23,14 @@ module DivisorUnitDP (clk,rst_n,usigned_n,divisor,dividend,reminder,quotient,div
   input reminder_en;
   input reminder_rShift;
   input quotient_en;
+  input counterMux_sel;
+  input count_upDown;
+  input count_load;
+  input count_en;
+  input counterReg_en;
+  output tc;
+  output signS;
+  output [1:0] magnitudeD;
   //signals
   logic [parallelism:0] signCorrection_to_DivisorReg;
   logic [parallelism:0] signCorrection_to_DividendReg;
@@ -54,7 +62,9 @@ module DivisorUnitDP (clk,rst_n,usigned_n,divisor,dividend,reminder,quotient,div
   logic [parallelism-1:0] rightAdder_to_outReg;
   logic [parallelism:0] leftAdder_to_outReg;
   logic [parallelism:0] reminderOutReg;
-
+  logic [5:0] counterMux_to_counter;
+  logic [5:0] counterOut_to_counterReg;
+  logic [5:0] counterRegOut;
   //begin describing architecture
 
   //dividing by 2 if datas are unsigned
@@ -71,6 +81,8 @@ module DivisorUnitDP (clk,rst_n,usigned_n,divisor,dividend,reminder,quotient,div
                                                     .shiftLeft(divisor_lShift),
                                                     .shiftRight(1'b0),
                                                     .newBit(1'b0));
+  assign magnitudeD = divisor_to_kernelLogic[parallelism:parallelism-1];
+
   //~divisor register
   register #(parallelism+1) notDivisorRegister (  .parallelIn(leftAdder_to_outReg),
                                                   .parallelOut(notDivisor_to_kernelLogic),
@@ -197,6 +209,7 @@ module DivisorUnitDP (clk,rst_n,usigned_n,divisor,dividend,reminder,quotient,div
                                                     .shiftLeft(1'b0),
                                                     .shiftRight(reminder_rShift),
                                                     .newBit(leftAdder_to_outReg[parallelism]));
+  assign signS = (reminderOutReg[parallelism]);
   assign reminder = (reminderOutReg[parallelism-1:0]);
   //quotient
   register #(parallelism) quotient (  .parallelIn(rightAdder_to_outReg),
@@ -205,5 +218,26 @@ module DivisorUnitDP (clk,rst_n,usigned_n,divisor,dividend,reminder,quotient,div
                                   .rst_n(rst_n),
                                   .clear(1'b0),
                                   .sample_en(quotient_en));
-  
+  //counter module
+  mux2to1 #(6) counterMux (  .inA(6'b000001),
+                              .inB(counterRegOut), //remember to set input carry
+                              .out(counterMux_to_counter),
+                              .sel(counterMux_sel));
+  syncCounter #(6) counter ( .clk(clk),
+                            .rst_n(rst_n),
+                            .clear(1'b0),
+                            .parallelLoad(counterMux_to_counter),
+                            .threashold(1'b000000),
+                            .upDown_n(count_upDown),
+                            .load_en(count_load),
+                            .cnt_en(count_en),
+                            .terminalCount(tc),
+                            .parallelOutput(counterOut_to_counterReg));
+  register #(6) counterReg (  .parallelIn(counterOut_to_counterReg),
+                              .parallelOut(counterRegOut),
+                              .clk(clk),
+                              .rst_n(rst_n),
+                              .clear(1'b0),
+                              .sample_en(counterReg_en));
+
 endmodule
